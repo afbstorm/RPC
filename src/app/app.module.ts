7715c1d53254f3919d54f9b277035a5135e0ea0a
afbstorm <aurelien.freyling@bstorm.be>
2023-08-29 11:38:32 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RulesComponent } from './rules/rules.component';
+import { GameComponent } from './game/game.component';
+import { ScoreComponent } from './score/score.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RulesComponent,
+    GameComponent,
+    ScoreComponent
   ],
   imports: [
     BrowserModule,
